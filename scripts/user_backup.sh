105e65f074fbd7ed00284496d670b85fbd25e894
Lucas Leone <lucasmleone@gmail.com>
2025-03-29 19:07:18 -0300

    first commit

---
+++ scripts/user_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BACKUP_DIR="../backups"
+DATE=$(date +%Y-%m-%d %H:%M:%S)
+BACKUP_NAME="user_db_backup_$DATE.sql"
+
+echo "$BACKUP_NAME REALIZADO Y CORRECTO" > $BACKUP_DIR/$BACKUP_NAME
+
+if [[$? -eq 0]]; then
+    echo "backup realizado CORRECTO"
+else
+    echo "hubo un fallo"
+    exit 1
+fi
