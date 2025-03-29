25f2dfa6db5b144b16f3fed0689d8e5cf5d5eed9
Lucas Leone <lucasmleone@gmail.com>
2025-03-29 19:44:23 -0300

    spaces problem

--- scripts/user_backup.sh
+++ scripts/user_backup.sh
@@ -6,7 +6,7 @@ BACKUP_NAME="user_db_backup_$DATE.sql"
 
 echo "$BACKUP_NAME REALIZADO Y CORRECTO" > $BACKUP_DIR/$BACKUP_NAME
 
-if [[$? -eq 0]]; then
+if [[ $? -eq 0 ]]; then
     echo "backup realizado CORRECTO"
 else
     echo "hubo un fallo"
